d4efe51fe0be5e49bba26dc84bb42f93ac7cd45e
congdeng <1829929@qq.com>
2020-04-30 01:43:00 -0400

    Traffic location updated.

--- data_wrangling.py
+++ data_wrangling.py
@@ -51,40 +51,52 @@ for key in traffic_data:
 traffic_locations = list(traffic_locations.keys())
 
 
-LOCATIONS = {}
-for item in construction_locations:
-    url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + item + '&key=' + API_KEY
-    r = requests.get(url)
-    response_dict=r.json()
-    if len(response_dict) != 0:
-        LOCATIONS[item] = [response_dict['results'][0]['formatted_address'], response_dict['results'][0]['geometry']['location']['lat'],
-                 response_dict['results'][0]['geometry']['location']['lng']]
+# TRAFFIC_LOCATIONS = {}
+# for item in traffic_locations:
+#     try:
+#         url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + item[0] + '&key=' +API_KEY
+#         r = requests.get(url)
+#         response_dict=r.json()
+#         if response_dict['status'] == 'OK':
+#             TRAFFIC_LOCATIONS[item] = [response_dict['results'][0]['formatted_address'], response_dict['results'][0]['geometry']['location']['lat'],
+#                      response_dict['results'][0]['geometry']['location']['lng']]
+#         url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + item[1] + '&key=' + API_KEY
+#         r = requests.get(url)
+#         response_dict = r.json()
+#         if response_dict['status'] == 'OK':
+#             TRAFFIC_LOCATIONS[item] = [response_dict['results'][0]['formatted_address'],
+#                    response_dict['results'][0]['geometry']['location']['lat'],
+#                    response_dict['results'][0]['geometry']['location']['lng']]
+#     except IndexError:
+#         pass
+#     continue
+# print('traffic location finished')
+# np.save("data/TRAFFIC_LOCATIONS.npy", TRAFFIC_LOCATIONS)
 
-# construction_header = ['[formatted_address, lat, lng]', 'WORK_START_DATE', 'WORK_END_DATE', 'PURPOSE']
+
+CONSTRUCTION_LOCATIONS = {}
+for item in construction_locations:
+    try:
+        url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + item + '&key=' + API_KEY
+        r = requests.get(url)
+        response_dict=r.json()
+        if response_dict['status'] == 'OK':
+            CONSTRUCTION_LOCATIONS[item] = [response_dict['results'][0]['formatted_address'], response_dict['results'][0]['geometry']['location']['lat'],
+                     response_dict['results'][0]['geometry']['location']['lng']]
+    except IndexError:
+        pass
+    continue
 print('construction locations finished')
 
-
-for item in traffic:
-    url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + item[0] + '&key=' +API_KEY
-    r = requests.get(url)
-    response_dict=r.json()
-    if len(response_dict) != 0:
-      LOCATIONS[item] = [response_dict['results'][0]['formatted_address'], response_dict['results'][0]['geometry']['location']['lat'],
-                 response_dict['results'][0]['geometry']['location']['lng']]
-    url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + item[1] + '&key=' + API_KEY
-    r = requests.get(url)
-    response_dict = r.json()
-    if len(response_dict) != 0:
-      LOCATIONS[item] = [response_dict['results'][0]['formatted_address'],
-               response_dict['results'][0]['geometry']['location']['lat'],
-               response_dict['results'][0]['geometry']['location']['lng']]
-print('traffic location finished')
+# construction_header = ['[formatted_address, lat, lng]', 'WORK_START_DATE', 'WORK_END_DATE', 'PURPOSE']
 # traffic_header = ['From[formatted_address, lat, lng]', 'To[formatted_address, lat, lng]', 'Direction', 'Date', '12:00-1:00 AM',
 #                   '1:00-2:00AM', '2:00-3:00AM', '3:00-4:00AM','4:00-5:00AM', '5:00-6:00AM', '6:00-7:00AM', '7:00-8:00AM',
 #                   '8:00-9:00AM', '9:00-10:00AM', '10:00-11:00AM','11:00-12:00PM', '12:00-1:00PM', '1:00-2:00PM', '2:00-3:00PM',
 #                   '3:00-4:00PM', '4:00-5:00PM', '5:00-6:00PM','6:00-7:00PM', '7:00-8:00PM', '8:00-9:00PM', '9:00-10:00PM',
 #                   '10:00-11:00PM', '11:00-12:00AM']
-np.save("LOCATIONS.npy", LOCATIONS)
+
+
+np.save("CONSTRUCTION_LOCATIONS.npy", CONSTRUCTION_LOCATIONS)
 
 
 
